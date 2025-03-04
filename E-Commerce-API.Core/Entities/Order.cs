763c85d06ea0115cb0bfc9b5b98b5fa07fdc81ad
Tarek-Ahmed-Helmy <tarek.ahmed35.01@gmail.com>
2025-03-04 22:09:01 +0200

    Code Review

--- E-Commerce-API.Core/Entities/Order.cs
+++ E-Commerce-API.Core/Entities/Order.cs
@@ -1,26 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace E_Commerce_API.Core.Entities;
 
-namespace E_Commerce_API.Core.Entities
+public class Order
 {
-    internal class Order
-    {
-        public int Id { get; set; }
-
-        public string OrderName { get; set; }
-        public string OrderStatus { get; set; }
-
-        public DateTime OrderDate { get; set; }
-
-        public int OrderPrice { get; set; }
-
-        public int OrderDetailsId { get; set; }
-
-        public OrderDetails OrderDetails { get; set; }
-
-        
-    }
+    public int Id { get; set; }
+    public string OrderStatus { get; set; }
+    public DateTime OrderDate { get; set; } = DateTime.Now;
+    public int OrderPrice { get; set; }
+    public int UserId { get; set; }
+    public User? User { get; set; }
 }
